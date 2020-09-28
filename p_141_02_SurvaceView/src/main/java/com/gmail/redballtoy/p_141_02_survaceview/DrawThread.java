4a7f58d334afd1d0cebfc28028df6a6ed29ee5ac
Dmitry Sorokin <redballtoy@gmail.com>
2020-09-28 10:30:37 +0300

    141_02

--- p_141_02_SurvaceView/src/main/java/com/gmail/redballtoy/p_141_02_survaceview/DrawThread.java
+++ p_141_02_SurvaceView/src/main/java/com/gmail/redballtoy/p_141_02_survaceview/DrawThread.java
@@ -4,10 +4,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.view.SurfaceHolder;
 
+//this is drawing thread in which drawing will be take place
 public class DrawThread extends Thread {
     private boolean running = false;
     private SurfaceHolder surfaceHolder;
 
+    //передается интерфейс SurfaceHolder что бы использовать Canvas
     public DrawThread(SurfaceHolder surfaceHolder) {
         this.surfaceHolder = surfaceHolder;
     }
@@ -19,9 +21,11 @@ public class DrawThread extends Thread {
     @Override
     public void run() {
         Canvas canvas;
+        //выполняется пока позволяет метка работы (running)
         while (running) {
             canvas = null;
             try {
+                //от SurfaceHolder получаем канву методом lockCanvas
                 canvas = surfaceHolder.lockCanvas(null);
                 if (canvas == null) {
                     continue;
@@ -30,6 +34,8 @@ public class DrawThread extends Thread {
                 }
             } finally {
                 if (canvas != null) {
+                    //возвращаем канву объекту SurfaceHolder методом unlockCanvasAndPost в секции
+                    //finally (обязательной для выполнения)
                     surfaceHolder.unlockCanvasAndPost(canvas);
                 }
             }
--- p_141_02_SurvaceView/src/main/java/com/gmail/redballtoy/p_141_02_survaceview/DrawView.java
+++ p_141_02_SurvaceView/src/main/java/com/gmail/redballtoy/p_141_02_survaceview/DrawView.java
@@ -18,39 +18,50 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
     private final String MY_LOG = "myLogs";
 
     /*In the DrawThread constructor, we get the SurfaceHolder handler and tell it that we
-    * will handle its events of which there are three:
-    *       surfaceCreated(@NonNull SurfaceHolder holder)
-    *       surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height)
-    *           -
-    *       surfaceDestroyed(@NonNull SurfaceHolder holder)
-    */
+     * will handle its events of which there are three:
+     *       surfaceCreated(@NonNull SurfaceHolder holder)
+     *       surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height)
+     *           -
+     *       surfaceDestroyed(@NonNull SurfaceHolder holder)
+     */
     public DrawView(Context context) {
         super(context);
         getHolder().addCallback(this);
     }
 
+    //SurfaceView is created and ready to display information
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        //creating your own drawing thread
         drawThread = new DrawThread(getHolder());
+        //put a label on the tread that it can run
         drawThread.setRunning(true);
+        //start the tread
         drawThread.start();
     }
 
+    //the format or size of the SurfaceView has been changed
     @Override
-    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+    public void surfaceChanged(@NonNull SurfaceHolder holder,
+                               int format, int width, int height) {
 
     }
 
+    //called before the SurfaceView is destroyed
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
         boolean retry = true;
+        //inform our thread that its work should be terminated
         drawThread.setRunning(false);
+        //stats a cycle that waits until the drawing thread exists
+        //it is necessary to wait so that the stream does not draw something on the
+        //destroyed SurfaceView
         while (retry) {
             try {
                 drawThread.join();
                 retry = false;
             } catch (InterruptedException e) {
-                Log.e(MY_LOG,e.toString());
+                Log.e(MY_LOG, e.toString());
             }
         }
     }
