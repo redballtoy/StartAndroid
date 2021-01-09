7ca709798f15d72ac8e7c8c9b0f7254376805f3f
Dmitry Sorokin <redballtoy@gmail.com>
2021-01-09 21:00:23 +0300

    p_028__01_intenyextras fin

---
+++ TestJavaHere/src/main/java/com/example/redballtoy/testjavahere/MainTestClass.java
@@ -0,0 +1,8 @@
+package com.example.redballtoy.testjavahere;
+
+public class MainTestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Java On Android");
+    }
+
+}
